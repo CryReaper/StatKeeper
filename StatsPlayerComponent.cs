83f8735464de3622d052c4f0a8205f8f921e6087
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-08-24 20:54:20 +0200

    ..

--- StatsPlayerComponent.cs
+++ StatsPlayerComponent.cs
@@ -1,12 +1,7 @@
 ﻿using Rocket.Core.Assets;
 using Rocket.Unturned.Player;
 using Steamworks;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StatKeeper
 {
